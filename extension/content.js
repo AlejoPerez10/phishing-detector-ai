e8b88b015fceb39e4fd7a69aa500fedcb8e174f4
Alejandro Perez <alejo.perezja@gmail.com>
2026-02-20 13:39:10 -0500

    Fixed the issue with the bucle when reading content from gmail

--- extension/content.js
+++ extension/content.js
@@ -1,30 +1,30 @@
+let lastEmailText = null;
+
 function getEmailText() {
-    const emailBody = document.querySelector("div.a3s"); 
-    if (emailBody) {
-        return emailBody.innerText;
-    }
-    return null;
+    const emailBody = document.querySelector("div.a3s");
+    return emailBody ? emailBody.innerText : null;
 }
 
 async function analyzeEmail(text) {
     const response = await fetch("https://phishing-detector-ai-qjz7.onrender.com/predict", {
         method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: text })
     });
 
     const data = await response.json();
 
     if (data.prediction === 1) {
-        alert("⚠️ Warning: This email may be phishing. Probability: " + data.probability.toFixed(2));
+        const percentage = (data.probability * 100).toFixed(2);
+        alert(`⚠️ Warning: Possible phishing.\nRisk: ${percentage}%`);
     }
 }
 
 const observer = new MutationObserver(() => {
     const text = getEmailText();
-    if (text) {
+
+    if (text && text !== lastEmailText) {
+        lastEmailText = text;
         analyzeEmail(text);
     }
 });
